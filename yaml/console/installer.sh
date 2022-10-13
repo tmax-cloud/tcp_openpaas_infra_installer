43b7f05470ebc89fb95fb0a14c0e3be747b350e0
jinsoo_youn <jinsoo_youn@tmax.co.kr>
2022-10-13 13:09:08 +0900

    [feat] update console

--- yaml/console/installer.sh
+++ yaml/console/installer.sh
@@ -11,9 +11,6 @@ function prepare_online(){
   sudo docker pull "tmaxcloudck/hypercloud-console:${CONSOLE_VER}"
   sudo docker save "tmaxcloudck/hypercloud-console:${CONSOLE_VER}" > "${install_dir}/tar/console_${CONSOLE_VER}.tar"
 
-	sudo docker pull "tmaxcloudck/console-operator:${OPERATOR_VER}"
-  sudo docker save "tmaxcloudck/hypercloud-console:${OPERATOR_VER}" > "${install_dir}/tar/operator_${OPERATOR_VER}.tar"
-
   sudo docker pull "jettech/kube-webhook-certgen:v1.3.0"
   sudo docker save "jettech/kube-webhook-certgen:v1.3.0" > "${install_dir}/tar/certgen_v1.3.0.tar"
 }
@@ -26,10 +23,6 @@ function prepare_offline(){
   sudo docker load < ${install_dir}/tar/console_${CONSOLE_VER}.tar
   sudo docker tag docker.io/tmaxcloudck/hypercloud-console:${CONSOLE_VER} ${REGISTRY}/tmaxcloudck/hypercloud-console:${CONSOLE_VER}
   sudo docker push ${REGISTRY}/tmaxcloudck/hypercloud-console:${CONSOLE_VER}
-
-  sudo docker load < ${install_dir}/tar/operator_${OPERATOR_VER}.tar
-  sudo docker tag docker.io/tmaxcloudck/console-operator:${OPERATOR_VER} ${REGISTRY}/tmaxcloudck/console-operator:${OPERATOR_VER}
-  sudo docker push ${REGISTRY}/tmaxcloudck/console-operator:${OPERATOR_VER}
 
   #tls 인증서 생성을 위한 도커 이미지 로드
   sudo docker load < ${install_dir}/tar/certgen_v1.3.0.tar
@@ -63,44 +56,10 @@ function install(){
     exit
   fi
 
-  if [[ $KIALI == "" ]]; then
-    KIALI_IP=$(kubectl get ingress kiali -n istio-system -o=jsonpath="{.status.loadBalancer.ingress[0].ip}")
-    KIALI_PORT="" # default https port = 433
-    if [[ -z $KIALI_IP ]]; then
-      echo "Cannot find Ingress KIALI_IP in istio-system. Is kiali installed on Ingress?"
-      KIALI_IP="0.0.0.0"
-      KIALI_PORT="20001"
-      echo "Failed to find KIALI, so KIALI=0.0.0.0:20001"
-    fi
-    KIALI=${KIALI_IP}:${KIALI_PORT}
-  fi
-
-  if [[ $KIBANA == "" ]]; then
-    KIBANA="kibana.kube-logging.svc.cluster.local:5601"
-  fi
-
-  if [[ $KUBEFLOW == "" ]]; then
-    KUBEFLOW="0.0.0.0"
-  fi
-
-  if [[ $GITLAB == "" ]]; then
-    GITLAB=$(kubectl -n gitlab-system exec -t $(kubectl -n gitlab-system get pod | grep gitlab | awk '{print $1}') -- cat /tmp/shared/omnibus.env 2>/dev/null | grep -oP "external_url '\K[^']*(?=')")
-    if [[ $GITLAB == "" ]]; then
-        echo "Failed to find GITLAB URL, so GITLAB=http://0.0.0.0"
-        GITLAB="http://0.0.0.0/"
-    fi
-  fi
-
-  if [[ $HYPERREGISTRY == "" ]]; then
-      echo "HYPERREGISTRY=http://0.0.0.0"
-      HYPERREGISTRY="http://0.0.0.0/"
-  fi
-  
   # Inject ENV into yaml
   rm -rf $temp_Dir
   cp -r $file_Dir $temp_Dir
   
-  sed -i "s%@@OPERATOR_VER@@%${OPERATOR_VER}%g" "${deploy_temp}"
   sed -i "s%@@CONSOLE_VER@@%${CONSOLE_VER}%g" "${deploy_temp}"
 
   sed -i "s%@@CLIENTID@@%${CLIENTID}%g" "${deploy_temp}"
@@ -108,12 +67,6 @@ function install(){
   sed -i "s%@@REALM@@%${REALM}%g" "${deploy_temp}"
 
   sed -i "s%@@MC_MODE@@%${MC_MODE}%g" "${deploy_temp}"
-
-  sed -i "s%@@KIALI@@%${KIALI}%g" "${deploy_temp}"
-  sed -i "s%@@KIBANA@@%${KIBANA}%g" "${deploy_temp}"
-  sed -i "s%@@KUBEFLOW@@%${KUBEFLOW}%g" "${deploy_temp}"
-  sed -i "s%@@GITLAB@@%${GITLAB}%g" "${deploy_temp}"
-  sed -i "s%@@HYPERREGISTRY@@%${HYPERREGISTRY}%g" "${deploy_temp}"
 
   if [[ "$REGISTRY" != "" ]]; then
     sed -i "s%docker.io%$REGISTRY/docker.io%g" "${job_temp}"
